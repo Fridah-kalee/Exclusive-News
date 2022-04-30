5f1376bd18486cba7061665229fc42be9a3aeee5
Fridah-kalee <fridahkalee@gmail.com>
2022-04-30 15:20:17 +0300

    instantiated objects in models.py

---
+++ app/models/articles.py
@@ -0,0 +1,13 @@
+class Articles:
+    '''
+    Articles class to define articles objects
+    '''
+    def __init__(self,source,author,title,description,url,urlToImage,publishedAt,content):
+        self.source = source
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage= urlToImage
+        self.publishedAt = publishedAt
+        self.content = content
