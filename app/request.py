9e27d74b375f26ed1c204c089b6e98c702057531
Fridah-kalee <fridahkalee@gmail.com>
2022-04-30 13:09:49 +0300

    Configured the base urls

---
+++ app/request.py
@@ -0,0 +1,9 @@
+from app import app
+
+#Getting api key
+api_key = app.config[NEWS_API_KEY]
+
+#Getting news base url
+base_url= app.config[NEWS_API_BASE_URL]
+articles_api_url= app.config[NEWS_ARTICLES_API_URL]
+source_articles_url= app.config[NEWS_SOURCE_ARTICLES_URL]
