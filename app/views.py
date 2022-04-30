55203e943d02d6f1a98e2fa9de4352d84f27bde2
Fridah-kalee <fridahkalee@gmail.com>
2022-04-30 08:03:00 +0300

    Launching our server

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from flask import render_template
 from app import app
 
 #views
-@app.route('/')
+@app.route('/')#localhost:5000/
 def FrontPage():
     '''
     View root page function that returns the front page and its data
