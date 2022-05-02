79fdc680d76c684b7626b759ee2052fa1d34bc45
Fridah-kalee <fridahkalee@gmail.com>
2022-05-02 20:31:35 +0300

    Deployment attempt

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     NEWS_API_BASE_URL='https://newsapi.org/v2/top-headlines?country=us&category={}&apiKey={}'
     NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
     NEWS_ARTICLES_API_URL='https://newsapi.org/v2/everything?q={}&apiKey={}'
-    
+    SECRET_KEY=os.environ.get('super-secret')
 
 class ProdConfig(Config):
     '''
--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 export NEWS_API_KEY='72f69442981742ed9a65115e3b252999'
+export SECRET_KEY='super-secret'
 python3.9 manage.py server
